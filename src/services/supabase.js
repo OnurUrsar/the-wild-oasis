999f8d5921e5e6bc68fa37115913371d34f61724
ursanor <89756520+OnurUrsar@users.noreply.github.com>
2024-01-10 15:27:47 +0300

    prefix for env

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 export const supabaseUrl = "https://dpzodorszbmukgyzxcgm.supabase.co";
-const supabaseKey = process.env.SUPABASE_KEY;
+const supabaseKey = process.env.VITE_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;

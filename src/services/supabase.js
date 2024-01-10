b3fbe5d33207161fc691301fbc029ac82306b34e
ursanor <89756520+OnurUrsar@users.noreply.github.com>
2024-01-10 14:42:57 +0300

    environment key

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 export const supabaseUrl = "https://dpzodorszbmukgyzxcgm.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRwem9kb3JzemJtdWtneXp4Y2dtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDA5MTMwNzcsImV4cCI6MjAxNjQ4OTA3N30.6tFXntmkZyuFOMOTi16sbbgrq_GdaKg_ej5Mx2Q6QwI";
+const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;

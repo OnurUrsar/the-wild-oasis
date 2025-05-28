41a57f7ef1f819081cbca75c11b1ef5ec85523b9
ursanor <89756520+OnurUrsar@users.noreply.github.com>
2025-05-28 21:52:23 +0300

    upload new data

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,6 +2,7 @@ import DashboardFilter from "../features/dashboard/DashboardFilter";
 import DashboardLayout from "../features/dashboard/DashboardLayout";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import Uploader from "../data/Uploader";
 
 function Dashboard() {
   return (
@@ -9,6 +10,7 @@ function Dashboard() {
       <Row type="horizontal">
         <Heading as="h1">Dashboard</Heading>
         <DashboardFilter />
+        <Uploader />
       </Row>
 
       <DashboardLayout />
